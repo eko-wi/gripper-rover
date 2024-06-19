e9f4343a89cf1940344fcb64728aa7e0024ea85a
eko-wi <kodosapiens@gmail.com>
2024-06-19 20:07:13 +0700

    Create esp32-transport-basic-PS3.ino

---
+++ esp32-transport-basic-PS3.ino
@@ -0,0 +1,44 @@
+/*
+PS3 stick data
+
+Buttons O= 2         Axis L : atas = - 502
+      X= 1                     kiri   = - 508
+kotak = 4                      kanan  = 512
+Segitiga = 8                   bawah = 512   
+
+Dpad 
+Panah atas = 1      Axis R : atas = - 502                                                                            
+Panah bawah = 2               kiri = - 508
+Panah kiri = 8                kanan = 512
+Panah kanan = 4               bawah = 512
+
+L1 = 0×10                      R2 throttle :1020 (analog?)
+L2 = 0×20                      L2 brake    :1020 (analog?)
+L1+L2 = 0×30
+
+Buttons 
+O = ( berputar ditempat ke arah kanan )
+
+|_| kotak =  (berputar ditempat ke arah kiri)
+
+/_\ Segitiga = ( maju )
+X = ( mundur )
+
+Dpad
+Panah atas ( menaikan lengan ke tinggi )
+Panah bawah ( menurunkan lengan ke rendah )
+Panah kiri ( menaikan lengan robot sedang )
+Panah kanan ( menaikan lengan robot sedang )
+
+L1 ( mencapit sekaligus melepaskan )
+L1+L2 (cit code)
+
+Axis L & R (-512 sampai +512, center 0 +- 5)
+Atas (maju)
+Kiri ( berbelok/memutar di tempat ke arah kiri )
+Kanan ( berbelok/memutar ditempat ke arah kanan )
+Bawah ( mundur )
+
+R Throttle = maju, 0-1024
+L Throttle = mundur, 0-1024
+*/

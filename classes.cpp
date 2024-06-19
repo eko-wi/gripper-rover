1944b649b90a2ec2629d8a570869d1e049e99aec
eko-wi <kodosapiens@gmail.com>
2024-06-19 19:32:26 +0700

    Update classes.cpp
    add sequencer and actionmanager

--- classes.cpp
+++ classes.cpp
@@ -61,7 +61,7 @@ class PIDController {
       KD = x;
     }
     void setIreset(int mode) {
-      resetimode = mode;
+      resetimode = mode;classes
     }
     void setTIreset(float t) {
       t_ = t;
@@ -117,9 +117,9 @@ class RateLimiter {
     long *timervar;
     long lastt, updateinterval;
   public:
-    RateLimiter(float maxrate, long *t) {
+    RateLimiter(float maxrate, long *Timervar) {
       r = maxrate;
-      timervar = t;
+      timervar = Timervar;
       posisisekarang = 0;
       updateinterval = 10;
     }
@@ -152,5 +152,128 @@ class RateLimiter {
         //Serial.println(selisih);
       }
       return posisisekarang;
+    }
+};
+class c_aksi {
+  private:
+    void (*gerakan)(long);
+  public:
+    long durasi;
+    c_aksi() {} //default constructor
+    void set_aksi(void (*Gerakan)(long), long Durasi) {
+      gerakan = Gerakan;
+      durasi = Durasi;
+    }
+    void exec(long t) {
+      gerakan(t);
+    }
+};
+
+#define MAX_AKSI 10
+class Sequencer {
+  private:
+    c_aksi aksi[MAX_AKSI];
+    long t1[MAX_AKSI]; //akhir dari setiap aksi
+    long t0;
+    int n, current;
+    bool isrunning;
+  public:
+    Sequencer(){ //contructor
+      t0=0;
+      n=0;
+      current=0;
+      isrunning=false;
+    }
+    void add(void (*g)(long), long durasi) { //fungsi g yang dilakukan memiliki satu parameter long (waktu)
+      aksi[n].set_aksi(g, durasi);
+      if (n == 0) {
+        t1[n] = durasi;
+      }
+      else {
+        t1[n] = t1[n - 1] + durasi;
+      }
+      n++;
+    }
+    void go(long t) {
+      //kalau aksi terakhir memiliki durasi -1 maka akan terus di-eksekusi tanpa akhir.
+      //kalau aksi terakhir memiliki durasi positif maka sesudah durasi itu habis, tidak ada eksekusi lagi
+      if (isrunning) {
+        if (current < n) {
+          if ((t < t1[current]) || (aksi[current].durasi < 0)) {
+            long t01 = t0;
+            if (current > 0) t01 = t1[current - 1];
+            aksi[current].exec(t - t01); //jalankan aksi ke-[current]
+          }
+          else { //durasi sudah habis, aksi berikutnya
+            current++; //aksi berikutnya
+          }
+        }
+        else { //sudah mencapai akhir daftar aksi
+          isrunning = false;
+        }
+      }
+    }
+    void jumpto(int i) {
+      current = i;
+    }
+    void stop(){
+      isrunning=false;
+    }
+    void restart() {
+      current = 0;
+      isrunning = true;
+      t0 = millis(); //waktu start
+    }
+};
+
+#define MAX_SEQUENCERS 8
+class ActionManager {
+  private:
+    int n;
+    long *t; //variabel timer yang diupdate di tempat lain, biasanya di program utama
+  public:
+    Sequencer* S[MAX_SEQUENCERS]; //array of pointers
+    ActionManager() {
+      n = 0;
+      t=NULL;
+    }
+    ActionManager(long *timervar) {
+      t = timervar;
+      n = 0;
+    }
+    void add(Sequencer *s) {
+      S[n] = s;
+      n++;
+    }
+    void settimer(long *timervar) {
+      t = timervar;
+    }
+    void clear() {
+      n = 0;
+    }
+    void start() { //menjalankan semua sequencer bersamaan
+      for (int i = 0; i < n; i++) {
+        S[i]->restart();
+      }
+    }
+    void start(int i){
+      S[i]->restart();
+    }
+    void stop(){ //menghentikan semua sequencer yang sedang berjalan
+      for (int i = 0; i < n; i++) {
+        S[i]->stop();
+      }
+    }      
+    void go() { //panggil fungsi ini di mainloop
+      long t1;
+      if (t != NULL) {
+        t1 = millis();
+      }
+      else {
+        t1 = *t;
+      }
+      for (int i = 0; i < n; i++) {
+        S[i]->go(t1);
+      }
     }
 };
